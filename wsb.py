ecb3834dd543e07edf408d7e7d13f19a51b01fc1
Ne00n <neo@tiefkuehler-madness.me>
2026-05-21 14:35:28 +0200

    loop to infinity boooyaaa

--- wsb.py
+++ wsb.py
@@ -3,14 +3,23 @@ import os
 tools = Base()
 
 path = os.path.dirname(os.path.realpath(__file__))
-origin = "https://old.reddit.com/r/wallstreetbets/"
 success, comments = tools.readFile(f"{path}/data/comments.json")
+origin = "https://old.reddit.com/r/wallstreetbets/"
 
-success, req = tools.fetch(origin)
-if not success: exit(f"Failed to fetch {origin}")
+while True:
+    print(f"Fetching {origin}")
+    success, req = tools.fetch(origin)
+    if not success: exit(f"Failed to fetch {origin}")
 
-urls = tools.getUrls(req.text)
-filteredUrls = tools.filterUrls(urls,"reddit.com/r/wallstreetbets/comments/")
-for url in filteredUrls: 
-    if url not in comments: comments.append(url)
-tools.saveFile(f"{path}/data/comments.json",filteredUrls)
+    urls = tools.getUrls(req.text)
+    filteredUrls = tools.filterUrls(urls,"reddit.com/r/wallstreetbets/comments/")
+    print(f"Fetched {len(filteredUrls)} comment urls")
+    for url in filteredUrls: 
+        if url not in comments: comments.append(url)
+    tools.saveFile(f"{path}/data/comments.json",comments)
+    nextPage = filteredUrls = tools.filterUrls(urls,"r/wallstreetbets/?count=")
+    if not nextPage: break
+    if len(nextPage) == 1:
+        origin = nextPage[0]
+    else:
+        origin = nextPage[1]
