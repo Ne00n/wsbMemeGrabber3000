0e71be75509b594814a3c2b60fbf856cccdb13bb
Ne00n <neo@tiefkuehler-madness.me>
2026-05-21 14:03:12 +0200

    append new comment urls

--- wsb.py
+++ wsb.py
@@ -4,11 +4,13 @@ tools = Base()
 
 path = os.path.dirname(os.path.realpath(__file__))
 origin = "https://old.reddit.com/r/wallstreetbets/"
-comments = tools.readFile(f"{path}/data/comments.json")
+success, comments = tools.readFile(f"{path}/data/comments.json")
 
 success, req = tools.fetch(origin)
 if not success: exit(f"Failed to fetch {origin}")
 
 urls = tools.getUrls(req.text)
 filteredUrls = tools.filterUrls(urls,"reddit.com/r/wallstreetbets/comments/")
+for url in filteredUrls: 
+    if url not in comments: comments.append(url)
 tools.saveFile(f"{path}/data/comments.json",filteredUrls)
